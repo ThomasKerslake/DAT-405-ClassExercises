ce813b8236b9532f500245b6511224ab64d07e24
ThomasKerslake <33869524+ThomasKerslake@users.noreply.github.com>
2017-11-29 17:16:20 +0000

    Update to Basic Animation

--- All Exercises/Animation/Animation1/sketch.js
+++ All Exercises/Animation/Animation1/sketch.js
@@ -1,11 +1,17 @@
 // Where is the circle
-var x, y;
+var x;
+var y;
+let size = 25;
+
+let r = 255;
+let g = 255;
+let b = 255;
 
 function setup() {
   createCanvas(720, 400);
   // Starts in the middle
-  x = width;
-  y = height / 2;
+  x = 0;
+  y = 0;
 }
 
 function draw() {
@@ -13,16 +19,22 @@ function draw() {
 
   // Draw a circle
   stroke(50);
-  fill(123,213,231);
-  ellipse(x, y, 24, 24);
+  fill(r,g,b);
+
+  ellipse(x, y, size, size);
 
   // Jiggling randomly on the horizontal axis
 
   // Moving up at a constant speed
-  x = x - 1;
+  x = x + 10;
+  y = y + 10;
 
   // Reset to the bottom
-  if (x < 0) {
-    x = width;
+  if (x > width && y > height) {
+    x = random(300,-300);
+    y = 0;
+    r = random(255);
+    g = random(255);
+    b = random(255);
   }
 }
