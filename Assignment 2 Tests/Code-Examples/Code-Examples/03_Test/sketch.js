46650fe03c8a08a211615c4424fd1a1680d415d9
ThomasKerslake <33869524+ThomasKerslake@users.noreply.github.com>
2018-01-08 17:20:08 +0000

    Update to assignment 2

--- Assignment 2 Tests/Code-Examples/Code-Examples/03_Test/sketch.js
+++ Assignment 2 Tests/Code-Examples/Code-Examples/03_Test/sketch.js
@@ -1,26 +1,32 @@
-//http://api.openweathermap.org
-
-//In this example, the weather variable is an array. Each position will store
-//an individual city. Therefore, by using a for loop we can load and process them all
 
 
-
+//set variables which will change the api query
+let sex = "male"; //male or female
+let country = "United Kingdom"; //from 1920 to 2059
+let date = "2017"; //http://api.population.io:80/1.0/countries
+let age = "19y1m"
 
 
 
 
 function setup() {
-  createCanvas(600, 600);
-  loadJSON("courts.json", gotData);
+  createCanvas(600,600);
+  //query the api using variables
+  loadJSON("http://api.population.io:80/1.0/life-expectancy/remaining/" + sex + "/" + country + "/" + date + "-01-01" + "/" + age + "/", gotData);
+  //set text size
   textSize(18);
   noLoop();
 }
 
 function gotData(data){
-  background(0);
-  for (var i = 0; i < data.main.temp; i++){
-    fill(255);
-    ellipse(random(width), random(height),16,16);
-    console.log("wewewewewe");
-  }
-    }
+  //set background and text colour
+background(0);
+  fill(255);
+  text("Sex: " + sex, width/2-160, height/2);
+  text("Country " + country, width/2-160, height/2 -120);
+  text("Current Date " + date, width/2-160, height/2 -90);
+  text("Current Age " + age, width/2-160, height/2 -60);
+  text("Your average remaining life expectancy left: " + data.remaining_life_expectancy, width/2-160, height/2 - 30);
+  //console log to see if the query has worked
+  console.log("query complete");
+}
