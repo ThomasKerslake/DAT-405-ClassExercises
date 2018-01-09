ec7cb1e4432a10c3e376bc715ba66187870bd8c4
ThomasKerslake <33869524+ThomasKerslake@users.noreply.github.com>
2018-01-09 11:29:21 +0000

    edit to assignment 2 test

--- Assignment 2 Tests/Code-Examples/Code-Examples/03_Test/sketch.js
+++ Assignment 2 Tests/Code-Examples/Code-Examples/03_Test/sketch.js
@@ -1,32 +1,49 @@
-
+var input, button, greeting;
 
 //set variables which will change the api query
-let sex = "male"; //male or female
-let country = "United Kingdom"; //from 1920 to 2059
-let date = "2017"; //http://api.population.io:80/1.0/countries
-let age = "19y1m"
+var sex = "male"; //male or female
+var country = "Brazil"; //from 1920 to 2059
+var date = "1989"; //http://api.population.io:80/1.0/countries
+var age = "19y"
 
 
 
 
 function setup() {
-  createCanvas(600,600);
+  createCanvas(1280,720);
   //query the api using variables
   loadJSON("http://api.population.io:80/1.0/life-expectancy/remaining/" + sex + "/" + country + "/" + date + "-01-01" + "/" + age + "/", gotData);
-  //set text size
-  textSize(18);
-  noLoop();
 }
+
 
 function gotData(data){
   //set background and text colour
 background(0);
   fill(255);
-  text("Sex: " + sex, width/2-160, height/2);
-  text("Country " + country, width/2-160, height/2 -120);
-  text("Current Date " + date, width/2-160, height/2 -90);
-  text("Current Age " + age, width/2-160, height/2 -60);
-  text("Your average remaining life expectancy left: " + data.remaining_life_expectancy, width/2-160, height/2 - 30);
+  input = createInput();
+  input.position(40, 65);
+  button = createButton('submit');
+  button.position(input.x + input.width + 10, 65);
+  button.mousePressed(sendData);
+  text("Give me your country?",40, 50);
+  textAlign(CENTER);
+  textSize(40);
+  text("Sex: " + sex, width/2, height/2+30);
+  text("Country: " + country, width/2, height/2 -130);
+  text("Year of Birth: " + date, width/2, height/2 -90);
+  text("Age: " + age, width/2, height/2 -50);
+  text("Your average remaining life expectancy left: " + data.remaining_life_expectancy, width/2, height/2 -10);
   //console log to see if the query has worked
   console.log("query complete");
+  }
+
+function draw (){
+  gotData(data);
+}
+
+function sendData() {
+  var country = input.value();
+  input.value('');
+  console.log(country);
+  redraw();
 }
